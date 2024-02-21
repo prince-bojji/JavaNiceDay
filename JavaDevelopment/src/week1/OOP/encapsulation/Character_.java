e447a3f7f23ff2dcb5871fc1d4829fb724708aa2
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2024-02-21 08:04:29 +0800

    fixed spelling error

---
+++ JavaDevelopment/src/week1/OOP/encapsulation/Character_.java
@@ -0,0 +1,45 @@
+package week1.OOP.encaptulation;
+
+//Class - act as the blueprint or template.
+public class Character_ {
+	/*
+	 * Attributes- these are the global variable inside the class.
+	 * Private attributes to avoid direct access in the main class.
+	 */
+	private String name;
+	private int hp;
+	private int atk;
+	private int def;
+	private int elementalMastery;
+	private int maxStamina;
+	
+	// Constructor - it is used to initialize the attributes of an object.
+	public Character_(int hp, int atk, int def, int elementalMastery, int maxStamina) {	
+		this.hp = hp;  // this keyword - refers to the class itself.
+		this.atk = atk;
+		this.def = def;
+		this.elementalMastery = elementalMastery;
+		this.maxStamina = maxStamina;
+	}
+	
+		// Object Methods - are methods inside the object class.
+		public void showAttributes() {
+			System.out.println("Name : "+name);
+			System.out.println("HP   : "+hp);
+			System.out.println("ATK  : "+atk);
+			System.out.println("DEF  : "+def);
+			System.out.println("Elemental Mastery : "+elementalMastery);
+			System.out.println("Max Stamina : "+maxStamina);
+		}
+		
+		// Setters - used to set encapsulated variables.
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		// Getters - used to get encapsulated variables.
+		public int getElementalMastery() { // You can get what ever you wanted using getters.
+			return elementalMastery;
+		}
+}
+

314b7403d9a2e49861a337dfcacb5c5fb733d98b
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2024-02-22 16:27:16 +0800

    remove solution for exercise 3

--- JavaDevelopment/src/week1/Exercises/submission/exercise4/jehan/Main.java
+++ JavaDevelopment/src/week1/Exercises/submission/exercise4/jehan/Main.java
@@ -1,33 +1,5 @@
 package week1.exercises.submission.exercise4.jehan;
 
 public class Main {
-    public static void main(String[] args) {
-        // First List
-        ListNode l1;
-        l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
-
-        // Second List
-        ListNode l2;
-        l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-
-        Solution solution = new Solution();
-        ListNode result = solution.addTwoNumbers(l1, l2);
-
-        System.out.println("Sum of two numbers:");
-        while (result != null) {
-            System.out.print(result.value);
-            if (result.next != null) {
-                System.out.print(" -> ");
-            }
-            result = result.next;
-        }
-    }
+    
 }
-
-
-
-

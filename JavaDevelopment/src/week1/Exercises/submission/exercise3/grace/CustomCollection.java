cfc8562b9fee2164aedbd07936a9344eaf1f29ad
websiteyarn <104047557+websiteyarn@users.noreply.github.com>
2024-02-23 14:36:20 +0800

    exercise 3 grace

---
+++ JavaDevelopment/src/week1/Exercises/submission/exercise3/grace/CustomCollection.java
@@ -0,0 +1,45 @@
+package week1.Exercises.submission.exercise3.grace;
+
+public class CustomCollection<Thing> {
+    Thing[] collection;
+    int size;
+
+    @SuppressWarnings("unchecked")
+    public CustomCollection(int size) {
+        collection = (Thing[]) new Object[size];
+        this.size = size;
+    }
+
+    public void add(Thing newElement) {
+        if (checkMax() == true) {
+            return;
+        }
+
+        int index = 0;
+        for (Thing x : collection) {
+            if (x == null) {
+                collection[index] = newElement;
+                break;
+            }
+            index++;
+        }
+    }
+
+    public void remove(int index) {
+        while (index != collection.length - 1) {
+            collection[index] = collection[index + 1];
+            index++;
+        }
+
+        collection[collection.length - 1] = null;
+    }
+
+    public Thing get(int index) {
+        return collection[index];
+    }
+
+    public boolean checkMax() {
+        return collection[collection.length - 1] != null;
+    }
+
+}
